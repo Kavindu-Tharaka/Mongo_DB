31f394614eb24c2ce7d128b510cbaf8a3fdd48c1
Tharaka W.C.M.K <it18001112@my.sliit.lk>
2021-09-07 18:06:12 +0530

    Learn advanced mongo querying

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ async function createCourse() {
   const course = new Course({
     name: "Mongo DB",
     author: "KTM",
-    author: 12.00,
+    price: 12.00,
     tags: ["mongodb", "database"],
     isPublished: true,
   });
@@ -69,15 +69,28 @@ async function getCourses(){
   // $in => in
   // $nin => not in
 
+  // or
+  // and
+
+  const pageNumber = 2;
+  const pageSize = 10;
+
+  // /api/v1/courses?pageNumber=2&pageSize=10
+
   const courses = await Course
-                          .find({price: {$gte: 10}})               // greater than or equal 10
-                          .find({price: {$gte: 10, $lt: 20}})      // greater than or equal 10 && lesser than 20
-                          .find({price: {$in: [10, 11, 14]} })     // equal to 10 or 11 or 14
-                          .limit(3)
+                          //.find({price: {$gte: 10}})               // greater than or equal 10
+                          //.find({price: {$gte: 10, $lt: 20}})      // greater than or equal 10 && lesser than 20
+                          //.find({price: {$in: [10, 11, 14]} })     // equal to 10 or 11 or 14
+                          .find()
+                          .or([{name: 'author'}, {price: 15.00}])
+
+                          .skip((pageNumber - 1) * pageSize)
+                          .limit(pageSize)
+
                           .sort({name: 1})
-                          .select({name: 1, author: 1});
+                          // .select({name: 1, author: 1})            // only return these attributes of returned docs.
+                          .count();                                   // count the # of documents returned
 
   console.log(courses);
 }
 
-getCourses();
